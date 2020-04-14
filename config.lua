55d10587789c79653c6478f814d9825efe098764
keiki <sexplosive42@gmail.com>
2020-04-15 04:49:14 +0700

    fixed spelling

--- config.lua
+++ config.lua
@@ -1,13 +1,13 @@
 Config = {}
 
-Config.BottleRecieve = { 1, 6 } -- This is the math random ex. math.random(1, 6) this will give you 1 - 6 bottles when searching a bin.
-Config.BottleReward = { 1, 4 } -- This is the math random ex. math.random(1, 4) this will give a random payout between 1 - 4
+Config.BottleReceive = {1, 6} -- This is the math random ex. math.random(1, 6) this will give you 1 - 6 bottles when searching a bin.
+Config.BottleReward = {1, 4} -- This is the math random ex. math.random(1, 4) this will give a random payout between 1 - 4
 
 -- Here you add all the bins you are going to search.
 Config.BinsAvailable = {
-    "prop_bin_01a",
-    "prop_bin_03a",
-    "prop_bin_05a"
+    'prop_bin_01a',
+    'prop_bin_03a',
+    'prop_bin_05a'
 }
 
 -- This is where you add the locations where you sell the bottles.
